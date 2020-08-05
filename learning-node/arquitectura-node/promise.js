8869e402dbcb839e71d78a989002284bf83c4503
Angel <angelcilfone@hotmail.com>
2020-08-04 23:50:26 -0300

    Arquitectura orientada a eventos

---
+++ learning-node/arquitectura-node/promise.js
@@ -0,0 +1,17 @@
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        if(Math.random() < 0.5){
+            resolve('hello world')
+        }else{
+            reject(new Error('Hello Error'))
+        }
+    }, 2000)
+})
+
+promise
+    .then(msg => msg.toUpperCase)
+    .then(msg => console.log('Message', msg))
+    .catch(err => console.log('error', err))
+
+
+
